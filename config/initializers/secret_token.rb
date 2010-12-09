51a2840fde6dd9757d3fb8ff80f64c816b91f504
Zahiduzzaman <thesetu@gmail.com>
2010-12-09 22:53:57 +0600

    :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UplaodPost::Application.config.secret_token = 'e9af3dafdad2ca56998eaa3cd38a05a6c5cd55b251e17e3ca6f42e9029fed51d4155177ec7ff55bac142a787de86991f65cdfea572358b2cf6c69e7106c9dd99'
